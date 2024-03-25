807d35590da18628d494af4f1224d98122b668e8
Jeisonbernardo <jeisontaboadamarcos@gmail.com>
2024-03-24 21:26:37 -0500

    segundo commit
    avance hasta el componente proyectos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { MainComponent } from './components/main/main.component';
+import { ServiciesComponent } from './components/servicies/servicies.component';
+import { SkillsComponent } from './components/skills/skills.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent,MainComponent,ServiciesComponent,SkillsComponent,ProjectsComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
